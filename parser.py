d5c51162321937a9ba3f59cd56126ef08143d2a9
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 18:43:24 -0400

    commit 9.2

--- parser.py
+++ parser.py
@@ -4,5 +4,5 @@ import subprocess
 
 def count_occurrances():
 	subprocess.run(['chmod', '+x', './log.sh'])
-	output = subprocess.check_output('./log.sh').encode().split()
+	output = subprocess.check_output('./log.sh').decode('utf-8').split()
 	print("addr:", output[1], "tries:", output[0])
