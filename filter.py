5922eb72bec3049335b1f7af5ffd331ddd5f8b65
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 17:10:58 -0400

    Second commit

--- filter.py
+++ filter.py
@@ -1,11 +1,6 @@
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-
-patterns = "*"
-ignore_patterns = ""
-ignore_directories = False
-case_sensitive = True
-event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+import subprocess
 
 def on_created(event):
     print("Log file created")
@@ -19,7 +14,30 @@ def on_modified(event):
 def on_moved(event):
     print("Log file moved")
 
-event_handler.on_created = on_created
-event_handler.on_deleted = on_deleted
-event_handler.on_modified = on_modified
-event_handler.on_moved = on_moved
+def start():
+	patterns = "*"
+	ignore_patterns = ""
+	ignore_directories = False
+	case_sensitive = True
+	event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+	event_handler.on_created = on_created
+	event_handler.on_deleted = on_deleted
+	event_handler.on_modified = on_modified
+	event_handler.on_moved = on_moved
+
+	path = "/var/log/auth.log"
+	go_recursively = True
+	observer = Observer()
+	observer.schedule(event_handler, path, recursive=go_recursively)
+
+	command = '''iptables -N LOGGING && iptables -A INPUT -j LOGGING && iptables -A LOGGING -m limit --limit 2/min -j LOG --log-prefix "IPTables-Dropped: " --log-level 4'''
+	subprocess.call([command])
+
+	observer.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+		observer.join()
---
+++ ids.py
@@ -0,0 +1,8 @@
+import filter
+import parser
+
+print("Starting script")
+
+if __name__ == "__main__":
+	filter.start()
+	parser.count_occurrances(0)
