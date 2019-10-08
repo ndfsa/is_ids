cc4bd2cd0bdfcc1816cba7476ca9c491010d00f3
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 18:36:50 -0400

    commit 9

--- log.sh
+++ log.sh
@@ -1,1 +1,2 @@
-tail -n 500 /var/log/auth.log | grep "Failed pass" | awk {print $11} | uniq -c | sort -nr
+#!/usr/bin/bash
+tail -n 500 /var/log/auth.log | grep "Failed pass" | awk '{print $11}' | uniq -c | sort -nr
--- parser.py
+++ parser.py
@@ -3,5 +3,6 @@ import re
 import subprocess
 
 def count_occurrances():
-	output = subprocess.check_output('bash log.sh')
+	subprocess.run(['chmod', '+x', './log.sh'])
+	output = subprocess.check_output('./log.sh')
 	print(output)
