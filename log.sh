7177e371397ffd969110f555f21ca94af57e652f
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 19:14:07 -0400

    alpha 1

--- log.sh
+++ log.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-tail -n 500 /var/log/auth.log | grep "Failed pass" | awk '{print $11}' | uniq -c | sort -nr
+tail -n 500 /var/log/auth.log | grep "Failed pass" | awk '{print $11}' | uniq -c | sort -nr | head -n 5
--- parser.py
+++ parser.py
@@ -1,8 +1,17 @@
-import datetime
-import re
 import subprocess
+import pickle
 
 def count_occurrances():
 	subprocess.run(['chmod', '+x', './log.sh'])
 	output = subprocess.check_output('./log.sh').decode('utf-8').split()
-	print("addr:", output[1], "tries:", output[0])
+	for i in range(len(output)):
+		if i % 2 == 0:
+			print("addr:", output[i + 1], "tries:", output[i])
+			if (int(output[i]) > 50):
+				with open("whitelist.txt", "rb") as fp:
+					whitelist = pickle.load(fp)
+					if (not output[i + 1] in whitelist):
+						subprocess.run(['iptables', '-D', 'INPUT', '-s', output[i + 1], '-j', 'DROP'])
+						print("Access restricted to", output[i + 1])
+					else:
+						print("forgot something?")
