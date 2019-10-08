5d11ee1c3582692e412632bc64277df5782dc9af
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 18:10:45 -0400

    commit 7.1

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import re
 import subprocess
 
 def count_occurrances():
-	command = ['tail -n 50 /var/log/auth.log', 'grep "Failed pass"', 'awk {print $11}', 'sort -nr']
+	command = ['tail -n 500 /var/log/auth.log', 'grep "Failed pass"', 'awk {print $11}', 'uniq -c','sort -nr']
 	process = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
 	for i in process.stdout:
 		out = i.split()
