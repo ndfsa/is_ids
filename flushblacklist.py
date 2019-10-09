21de3fb97cb9193f74c4065ad08f9362305f4ba6
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-10-08 21:01:54 -0400

    v1.0

--- flushblacklist.py
+++ flushblacklist.py
@@ -1,11 +1,17 @@
 import pickle
 import subprocess
+import sys
 
 if __name__ == "__main__":
 	blacklist = []
-	
-	with open("blacklist.txt", "wb") as fp:
-		pickle.dump(blacklist, fp)
-		subprocess.run(['iptables', '-F'])
-		print(blacklist)
-		fp.close()
+	if (str(sys.argv[1]) == "-f"):
+		with open("blacklist.txt", "wb") as fp:
+			pickle.dump(blacklist, fp)
+			subprocess.run(['iptables', '-F'])
+			print(blacklist)
+			fp.close()
+	elif (str(sys.argv[1]) == "-p"):
+		with open("blacklist.txt", "rb") as fp:
+			blacklist = pickle.load(fp)
+			print(blacklist)
+			fp.close()
